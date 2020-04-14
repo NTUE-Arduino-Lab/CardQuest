3cbaead40348303bc34277366da54764243843b1
e2ne0 <e2ne00@gmail.com>
2020-04-14 15:26:18 +0800

    forward timer

---
+++ Assets/Scripts/MasterCtrl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class MasterCtrl : MonoBehaviour
+{   
+    public int status;
+    // float time;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            status = 1;
+        }
+        switch(status)
+        {
+            case 0:
+                
+                break;
+            case 1:
+                InvokeRepeating("forward1",2.0f,1.0f);
+                status = 0;
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+        }
+    }
+    public void forward1()
+    {   
+        this.transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SceneCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneCtrl : MonoBehaviour
+{
+    public GameObject master;
+    private GameObject startPoint;
+    private GameObject finishPoint;
+    public GameObject VictoryObj;
+    void Start()
+    {
+        startPoint = GameObject.FindGameObjectWithTag("Start Point");
+        finishPoint = GameObject.FindGameObjectWithTag("Finish Point");
+        // print("Start Point is: " + startPoint.name);
+        // print("Finish Point is: " + finishPoint.name);
+        float masterStartPointX = startPoint.transform.position.x;
+        float masterStartPointZ = startPoint.transform.position.z;
+        master.transform.position = new Vector3(masterStartPointX,0.5f,masterStartPointZ);
+        master.SetActive(true);
+    }
+    void Update()
+    {
+        if(master.transform.position.x == finishPoint.transform.position.x &&
+        master.transform.position.z == finishPoint.transform.position.z)
+        {
+            Win();
+        }
+    }
+    void Win()
+    {
+        VictoryObj.SetActive(true);
+    }
+}
