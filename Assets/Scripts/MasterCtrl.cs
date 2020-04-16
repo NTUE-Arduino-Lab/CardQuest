856cfc9774da02f8d9339318b324ac4b46199e18
e2ne0 <e2ne00@gmail.com>
2020-04-16 12:51:27 +0800

    wait 4 art

--- Assets/Scripts/MasterCtrl.cs
+++ Assets/Scripts/MasterCtrl.cs
@@ -1,94 +1,181 @@
 ﻿using UnityEngine;
-
+using System.Collections.Generic;
 public class MasterCtrl : MonoBehaviour
 {
-    public int status;
-    public float speed;
-    private float startTime;
-    private bool getTarget;
-    private Vector3 targetPoint;
-    private Quaternion rotateTargetPoint;
-    void Start()
-    {
+	public int status;
+	public int steps;
+	public float speed;
+	private float startTime;
+	private bool getTarget;
+	private Vector3 targetPoint;
+	private Quaternion rotateTargetPoint;
+	public List<int> act = new List<int>();
+	public List<int> tempList = new List<int>();
+	public bool go;
+	void Start()
+	{
 
-    }
-    void Update()
-    {
-
-        switch (status)
-        {
-            case 0:
-                if (Input.GetKeyUp(KeyCode.W))
-                {
-                    status = 1;
-                }
-                if (Input.GetKeyUp(KeyCode.A))
-                {
-                    status = 2;
-                }
-                if (Input.GetKeyUp(KeyCode.D))
-                {
-                    status = 3;
-                }
-                this.transform.rotation = rotateTargetPoint;
-                startTime = Time.time;
-                getTarget = false;
-                break;
-            case 1:
-                forward1();
-                break;
-            case 2:
-                turnLeft();
-                break;
-            case 3:
-                turnRight();
-                break;
-        }
-    }
-    public void forward1()
-    {
-        float distCovered = (Time.time - startTime) * speed;
-        Vector3 startPoint = this.transform.position;
-        if (!getTarget)
-        {
-            targetPoint = this.transform.position + this.transform.forward * 2;
-            getTarget = true;
-        }
-        this.transform.position = Vector3.Lerp(startPoint, targetPoint, distCovered);
-        if (this.transform.position == targetPoint)
-        {
-            status = 0;
-        }
-    }
-
-    public void turnLeft()
-    {
-        float distCovered = (Time.time - startTime) * speed;
-        Quaternion rotateStartPoint = this.transform.rotation;
-        if (!getTarget)
-        {
-            rotateTargetPoint = Quaternion.Euler(0.0f, this.transform.rotation.eulerAngles.y - 90f, 0.0f);
-            getTarget = true;
-        }
-        this.transform.rotation = Quaternion.Lerp(rotateStartPoint, rotateTargetPoint, distCovered);
-        if (this.transform.rotation.eulerAngles == rotateTargetPoint.eulerAngles)
-        {
-            status = 0;
-        }
-    }
-    public void turnRight()
-    {
-        float distCovered = (Time.time - startTime) * speed;
-        Quaternion rotateStartPoint = this.transform.rotation;
-        if (!getTarget)
-        {
-            rotateTargetPoint = Quaternion.Euler(0.0f, this.transform.rotation.eulerAngles.y + 90f, 0.0f);
-            getTarget = true;
-        }
-        this.transform.rotation = Quaternion.Lerp(rotateStartPoint, rotateTargetPoint, distCovered);
-        if (this.transform.rotation.eulerAngles == rotateTargetPoint.eulerAngles)
-        {
-            status = 0;
-        }
-    }
+	}
+	void Update()
+	{
+		switch (status)
+		{
+			case 0:
+				tempLoopEnd = false;
+				if (!go)
+				{
+					if (Input.GetKeyUp(KeyCode.W))
+						getForward();
+					if (Input.GetKeyUp(KeyCode.A))
+						getLeft();
+					if (Input.GetKeyUp(KeyCode.D))
+						getRight();
+					if (Input.GetKeyUp(KeyCode.Space))
+						getGo();
+					if (Input.GetKeyUp(KeyCode.L))
+						status = 5;
+					if (Input.GetKeyUp(KeyCode.K))
+						status = 4;
+				}
+				if (go)
+				{
+					this.transform.rotation = rotateTargetPoint;
+					startTime = Time.time;
+					getTarget = false;
+					if (steps < act.Count)
+						status = act[steps];
+				}
+				break;
+			case 1:
+				forward();
+				break;
+			case 2:
+				turnLeft();
+				break;
+			case 3:
+				turnRight();
+				break;
+			case 4:
+				addLoop(2);
+				if (Input.GetKeyUp(KeyCode.W))
+					getForward();
+				if (Input.GetKeyUp(KeyCode.A))
+					getLeft();
+				if (Input.GetKeyUp(KeyCode.D))
+					getRight();
+				if (Input.GetKeyUp(KeyCode.Space))
+					getEndLoop();
+				break;
+			case 5:
+				addLoop(3);
+				if (Input.GetKeyUp(KeyCode.W))
+					getForward();
+				if (Input.GetKeyUp(KeyCode.A))
+					getLeft();
+				if (Input.GetKeyUp(KeyCode.D))
+					getRight();
+				if (Input.GetKeyUp(KeyCode.Space))
+					getEndLoop();
+				break;
+		}
+	}
+	private void forward()
+	{
+		float distCovered = (Time.time - startTime) * speed;
+		Vector3 startPoint = this.transform.position;
+		if (!getTarget)
+		{
+			targetPoint = this.transform.position + this.transform.forward * 2;
+			getTarget = true;
+		}
+		this.transform.position = Vector3.Lerp(startPoint, targetPoint, distCovered);
+		if (this.transform.position == targetPoint)
+		{
+			steps++;
+			status = 0;
+		}
+	}
+	public void getForward()
+	{
+		if (status == 0)
+			act.Add(1);
+		else if (status >= 4)
+			tempList.Add(1);
+	}
+	private void turnLeft()
+	{
+		float distCovered = (Time.time - startTime) * speed;
+		Quaternion rotateStartPoint = this.transform.rotation;
+		if (!getTarget)
+		{
+			rotateTargetPoint = Quaternion.Euler(0.0f, this.transform.rotation.eulerAngles.y - 90f, 0.0f);
+			getTarget = true;
+		}
+		this.transform.rotation = Quaternion.Lerp(rotateStartPoint, rotateTargetPoint, distCovered);
+		if (this.transform.rotation.eulerAngles == rotateTargetPoint.eulerAngles)
+		{
+			steps++;
+			status = 0;
+		}
+	}
+	public void getLeft()
+	{
+		if (status == 0)
+			act.Add(2);
+		else if (status >= 4)
+			tempList.Add(2);
+	}
+	private void turnRight()
+	{
+		float distCovered = (Time.time - startTime) * speed;
+		Quaternion rotateStartPoint = this.transform.rotation;
+		if (!getTarget)
+		{
+			rotateTargetPoint = Quaternion.Euler(0.0f, this.transform.rotation.eulerAngles.y + 90f, 0.0f);
+			getTarget = true;
+		}
+		this.transform.rotation = Quaternion.Lerp(rotateStartPoint, rotateTargetPoint, distCovered);
+		if (this.transform.rotation.eulerAngles == rotateTargetPoint.eulerAngles)
+		{
+			steps++;
+			status = 0;
+		}
+	}
+	public void getRight()
+	{
+		if (status == 0)
+			act.Add(3);
+		else if (status >= 4)
+			tempList.Add(3);
+	}
+	private bool tempLoopEnd;
+	public void getEndLoop()
+	{
+		tempLoopEnd = true;
+	}
+	public void getLoop2()
+	{
+		status = 4;
+	}
+	public void getLoop3()
+	{
+		status = 5;
+	}
+	private void addLoop(int loopStep)
+	{
+		if(tempLoopEnd == true)
+		{
+			for (int i = 0; i < loopStep; i++)
+			{
+				for (int j = 0; j < tempList.Count; j++)
+					act.Add(tempList[j]);
+			}
+			tempList.Clear();
+			status = 0;
+		}
+	}
+	public void getGo()
+	{
+		go = true;
+	}
 }
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    List<int> mList = new List<int>();
+    void Start()
+    {
+        mList.Add(5);
+        int temp;
+        temp = mList[0];
+        print(temp);
+        
+    }
+    void Update()
+    {
+        
+    }
+}
